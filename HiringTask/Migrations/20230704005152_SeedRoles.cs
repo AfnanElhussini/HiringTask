2f63837fab97ccc57cf641d5fdd3ab007877f3f5
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-07-04 04:04:46 +0300

    Seed Database with needed Roles

---
+++ HiringTask/Migrations/20230704005152_SeedRoles.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiringTask.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] {Guid.NewGuid().ToString(), "User", "User".ToUpper(), Guid.NewGuid().ToString()}
+                );
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] {Guid.NewGuid().ToString(), "Admin", "Admin".ToUpper(), Guid.NewGuid().ToString()}
+                                                             );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [dbo].[AspNetRoles]");
+        }
+      
+
+    }
+}
